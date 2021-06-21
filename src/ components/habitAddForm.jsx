af7bac7d261dce05cc57e83ad4ffa6465ee898b5
ByungyeonKim <viva_kby@naver.com>
2021-06-21 23:23:11 +0900

    Update habitAddForm.jsx

--- src/ components/habitAddForm.jsx
+++ src/ components/habitAddForm.jsx
@@ -1,26 +1,20 @@
 import React, { memo } from 'react';
 
-const HabitAddForm = memo(props => {
+const HabitAddForm = memo((props) => {
   const formRef = React.createRef();
   const inputRef = React.createRef();
 
-  const onSubmit = event => {
+  const onSubmit = (event) => {
     event.preventDefault();
     const name = inputRef.current.value;
     name && props.onAdd(name);
     formRef.current.reset();
   };
-  console.log("dd?");
 
   return (
-    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
-      <input 
-        ref={inputRef}
-        type="text" 
-        className="add-input" 
-        placeholder="습관을 입력하세요.
-      "/>
-      <button className="add-button">Add</button>
+    <form ref={formRef} className='add-form' onSubmit={onSubmit}>
+      <input ref={inputRef} type='text' className='add-input' placeholder='습관을 입력하세요.' />
+      <button className='add-button'>Add</button>
     </form>
   );
 });
